73f627030a92a174609a1766068125b113d07116
Antoine DESRUET <antoine.desruet@epitech.eu>
2023-03-30 18:49:47 +0900

    feat: problem1 (1 et 2)

---
+++ proj1/problem1/pc_static_block.java
@@ -0,0 +1,60 @@
+class pcThread extends Thread {
+    int begin;
+    int end;
+    int result = 0;
+
+    pcThread(int b, int e) {
+        begin = b;
+        end = e;
+    }
+
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        for (int i = begin; i < end; ++i) {
+            result += isPrime(i);
+        }
+        long endTime = System.currentTimeMillis();
+        long timeDiff = endTime - startTime;
+        System.out.printf("Execution Time of thread %d : %d ms\n", Thread.currentThread().getId(), timeDiff);
+    }
+
+    private static int isPrime(int x) {
+        if ((x <= 1) || (x & 1) == 0)
+            return 0;
+        for (int i = 3; i < x; i++) {
+            if ((x % i == 0) && (i != x))
+                return 0;
+        }
+        return 1;
+    }
+}
+
+class pc_static_bloc {
+    private static int NUM_END = 200000;
+    private static int N_THREAD = 1;
+
+    public static void main(String[] args) {
+        int result = 0;
+        if (args.length == 2) {
+            N_THREAD = Integer.parseInt(args[0]);
+            NUM_END = Integer.parseInt(args[1]);
+        }
+        pcThread[] threads = new pcThread[N_THREAD];
+        int range = NUM_END / N_THREAD;
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < threads.length; ++i) {
+            threads[i] = new pcThread(i * range, i * range + range);
+            threads[i].start();
+        }
+        for (int i = 0; i < threads.length; ++i) {
+            try {
+                threads[i].join();
+                result += threads[i].result;
+            } catch(InterruptedException e) {}
+        }
+        long endTime = System.currentTimeMillis();
+        long timeDiff = endTime - startTime;
+        System.out.printf("Execution Time : %d ms\n", timeDiff);
+        System.out.printf("1... %d prime# counter == %d\n",NUM_END - 1, result);
+    }
+}
---
+++ proj1/problem1/pc_static_cyclic.java
@@ -0,0 +1,68 @@
+class pcThread extends Thread {
+    int begin;
+    int end;
+    int task_size;
+    int n_thread;
+    int result = 0;
+
+    pcThread(int b, int e, int o, int n) {
+        begin = b;
+        end = e;
+        task_size = o;
+        n_thread = n;
+    }
+
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        
+        for (int j = begin; begin < end; begin += task_size * n_thread) {
+            for (int i = begin; i < begin + task_size; ++i) {
+                result += isPrime(i);
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        long timeDiff = endTime - startTime;
+        System.out.printf("Execution Time of thread %d : %d ms\n", Thread.currentThread().getId(), timeDiff);
+    }
+
+    private static int isPrime(int x) {
+        if ((x <= 1) || (x & 1) == 0)
+            return 0;
+        for (int i = 3; i < x; i++) {
+            if ((x % i == 0) && (i != x))
+                return 0;
+        }
+        return 1;
+    }
+}
+
+class pc_static_cyclic {
+    private static int NUM_END = 200000;
+    private static int N_THREAD = 10;
+    private static int TASK_SIZE = 10;
+
+    public static void main(String[] args) {
+        int result = 0;
+        if (args.length == 3) {
+            N_THREAD = Integer.parseInt(args[0]);
+            NUM_END = Integer.parseInt(args[1]);
+            TASK_SIZE = Integer.parseInt(args[2]);
+        }
+        pcThread[] threads = new pcThread[N_THREAD];
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < threads.length; ++i) {
+            threads[i] = new pcThread(i * TASK_SIZE, NUM_END, TASK_SIZE, N_THREAD);
+            threads[i].start();
+        }
+        for (int i = 0; i < threads.length; ++i) {
+            try {
+                threads[i].join();
+                result += threads[i].result;
+            } catch(InterruptedException e) {}
+        }
+        long endTime = System.currentTimeMillis();
+        long timeDiff = endTime - startTime;
+        System.out.printf("Execution Time : %d ms\n", timeDiff);
+        System.out.printf("1... %d prime# counter == %d\n",NUM_END - 1, result);
+    }
+}
