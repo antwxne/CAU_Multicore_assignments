80f903b6600b8a60ff08ab81df08737541983964
Antoine DESRUET <antoine.desruet@epitech.eu>
2023-05-16 11:25:20 +0900

    feat: data in report

--- 50221600_proj3/prob2/benchmark/benchmark.py
+++ 50221600_proj3/prob2/benchmark/benchmark.py
@@ -11,9 +11,12 @@ CHUNK_SIZE: list[int] = [1, 5, 10, 100]
 OUTPUT_FOLDER: str = "./benchmark/results"
 HEADERS_EXEC_TIME: list[str] = ["Number of thread",
                                 *map(lambda x: f"Execution time {x} chunks",
-                                     CHUNK_SIZE),
-                                *map(lambda x: f"Performance {x} chunks",
                                      CHUNK_SIZE)]
+
+HEADERS_PERFORMANCE: list[str] = ["Number of thread",
+                                  *map(lambda x: f"Performance {x} chunks",
+                                       CHUNK_SIZE)]
+
 OUTPUT_NAMES: dict[int, str] = {
     1: "static",
     2: "dynamic",
@@ -21,8 +24,9 @@ OUTPUT_NAMES: dict[int, str] = {
 }
 
 
-def benchmark(scheduling_type: int) -> list[list]:
-    dest: list[list] = []
+def benchmark(scheduling_type: int) -> tuple[list[list], list[list]]:
+    result_exec_time: list[list] = []
+    result_performance: list[list] = []
     for t in tqdm(THREADS_NUMBER):
         chunk_results_exec: list = []
         chunk_results_performance: list = []
@@ -36,11 +40,12 @@ def benchmark(scheduling_type: int) -> list[list]:
             performance: float = 1 / time_ms
             chunk_results_exec.append(time_ms)
             chunk_results_performance.append(performance)
-        dest.append([t, *chunk_results_exec, *chunk_results_performance])
-    return dest
+        result_exec_time.append([t, *chunk_results_exec])
+        result_performance.append([t, *chunk_results_performance])
+    return result_exec_time, result_performance
 
 
-def plot(data: pd.DataFrame, scheduling_type: int) -> None:
+def plot_exec_time(data: pd.DataFrame, scheduling_type: int) -> None:
     plt.plot(data[HEADERS_EXEC_TIME[0]], data[HEADERS_EXEC_TIME[1]], color='r',
              label="1 chunk", marker="x")
     plt.plot(data[HEADERS_EXEC_TIME[0]], data[HEADERS_EXEC_TIME[2]], color='g',
@@ -56,13 +61,20 @@ def plot(data: pd.DataFrame, scheduling_type: int) -> None:
     plt.savefig(
         f'{OUTPUT_FOLDER}/{OUTPUT_NAMES[scheduling_type]}_exec_time.png')
     plt.close()
-    plt.plot(data[HEADERS_EXEC_TIME[0]], data[HEADERS_EXEC_TIME[5]], color='r',
+
+
+def plop_performance(data: pd.DataFrame, scheduling_type: int) -> None:
+    plt.plot(data[HEADERS_PERFORMANCE[0]], data[HEADERS_PERFORMANCE[1]],
+             color='r',
              label="1 chunk", marker="x")
-    plt.plot(data[HEADERS_EXEC_TIME[0]], data[HEADERS_EXEC_TIME[6]], color='g',
+    plt.plot(data[HEADERS_PERFORMANCE[0]], data[HEADERS_PERFORMANCE[2]],
+             color='g',
              label="5 chunks", marker="x")
-    plt.plot(data[HEADERS_EXEC_TIME[0]], data[HEADERS_EXEC_TIME[7]], color='b',
+    plt.plot(data[HEADERS_PERFORMANCE[0]], data[HEADERS_PERFORMANCE[3]],
+             color='b',
              label="10 chunks", marker="x")
-    plt.plot(data[HEADERS_EXEC_TIME[0]], data[HEADERS_EXEC_TIME[8]], color='k',
+    plt.plot(data[HEADERS_PERFORMANCE[0]], data[HEADERS_PERFORMANCE[4]],
+             color='k',
              label="100 chunks", marker="x")
     plt.title("Performance in function of the number of thread")
     plt.xlabel("# Thread")
@@ -73,14 +85,21 @@ def plot(data: pd.DataFrame, scheduling_type: int) -> None:
     plt.close()
 
 
-#
 if __name__ == "__main__":
     if not os.path.exists(OUTPUT_FOLDER):
         os.makedirs(OUTPUT_FOLDER)
     for scheduling_type in OUTPUT_NAMES.keys():
-        results = benchmark(scheduling_type)
-        data = pd.DataFrame(results, columns=HEADERS_EXEC_TIME)
-        data.to_csv(f'{OUTPUT_FOLDER}/{OUTPUT_NAMES[scheduling_type]}.csv',
-                    index=False)
+        result_exec_time, result_performance = benchmark(scheduling_type)
+        data_exec_time = pd.DataFrame(result_exec_time,
+                                      columns=HEADERS_EXEC_TIME)
+        data_exec_time.to_csv(
+            f'{OUTPUT_FOLDER}/{OUTPUT_NAMES[scheduling_type]}_exec_time.csv',
+            index=False)
+        data_performance = pd.DataFrame(result_performance,
+                                        columns=HEADERS_PERFORMANCE)
+        data_performance.to_csv(
+            f'{OUTPUT_FOLDER}/{OUTPUT_NAMES[scheduling_type]}_performance.csv',
+            index=False)
         # results = pd.read_csv(f'{OUTPUT_FOLDER}/{OUTPUT_NAMES[1]}.csv', header=None, skiprows=[0])
-        plot(data, scheduling_type)
+        plot_exec_time(data_exec_time, scheduling_type)
+        plop_performance(data_performance, scheduling_type)
